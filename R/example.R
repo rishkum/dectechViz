af634fb827888162c2fe95b81267f85ed83daf4f
Rishabh Kumar <kumarrishabh14@hotmail.com>
2020-05-26 16:31:39 +0100

    Update example.R

--- R/example.R
+++ R/example.R
@@ -4,7 +4,7 @@ p1 <- ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
   geom_point(size = 4) + geom_smooth(method=lm,se=FALSE, aes(group=1), ) +
   scale_color_dectech()
 
-p1 <- ggpretay(p1, title= "Colour", xaxis = "coloir", yaxis = "vof")
+#p1 <- ggpretay(p1, title= "Colour", xaxis = "coloir", yaxis = "vof")
 
 gg_check(p1)
 
